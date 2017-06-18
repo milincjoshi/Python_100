6d7f97a1150f542f1a8e38e59114b34bab6e2898
Milin Joshi <milinjoshi@Milins-MacBook-Air.local>
2017-06-18 14:10:15 -0700

    15

---
+++ 15.py
@@ -0,0 +1,19 @@
+'''
+Question:
+Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+Suppose the following input is supplied to the program:
+9
+Then, the output should be:
+11106
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+'''
+
+n = int(raw_input())
+ans = 0
+num = n
+for x in range(1,5):
+	ans+=num
+	num = int(str(num)+ str(n))
+print ans
