c9f21017e984f160860a49eb22ebc4e5a79dd612
Milin Joshi <milincjoshi@gmail.com>
2017-06-20 22:20:52 -0700

    29

---
+++ 29.py
@@ -0,0 +1,12 @@
+'''
+Question:
+Define a function that can accept two strings as input and concatenate them and then print it in console.
+
+Hints:
+
+Use + to concatenate the strings
+'''
+
+def con(a,b):
+	return a+b
+print con("Milin","Joshi")
