fec9c591f9858311c228aaac68655bca537a830f
Milin Joshi <milincjoshi@gmail.com>
2017-06-20 23:16:32 -0700

    32

---
+++ 32.py
@@ -0,0 +1,15 @@
+'''
+Question:
+Define a function which can print a dictionary where the keys are numbers 
+between 1 and 3 (both included) and the values are square of keys.
+
+Hints:
+
+Use dict[key]=value pattern to put entry into a dictionary.
+Use ** operator to get power of a number.
+'''
+
+d = {}
+for x in range(1,4):
+	d[x] = x*x
+print d
