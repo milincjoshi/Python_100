ea0080d5c10ac6ae4e81971ad9361a7a2bcf0647
Milin Joshi <milincjoshi@gmail.com>
2017-06-20 22:25:37 -0700

    31

---
+++ 31.py
@@ -0,0 +1,16 @@
+'''
+Question:
+Define a function that can accept an integer number as input and print the "It is an even number" 
+if the number is even, otherwise print "It is an odd number".
+
+Hints:
+
+Use % operator to check if a number is even or odd.
+'''
+def even_or_odd(n):
+	if n%2==0:
+		print "It is an odd number"
+	else:
+		print "It is an even number"
+even_or_odd(2)
+even_or_odd(3)
