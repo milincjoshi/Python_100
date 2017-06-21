10955b1b93f964777599096474b4481160176ebb
Milin Joshi <milincjoshi@gmail.com>
2017-06-20 22:17:50 -0700

    27

---
+++ 27.py
@@ -0,0 +1,18 @@
+'''
+Question:
+Define a function that can convert a integer into a string and print it in console.
+
+Hints:
+
+Use str() to convert a number to string.
+'''
+
+def conv(a):
+	return str(a)
+
+a = 3
+ans = conv(a)
+print type(a)
+print type(ans)
+
+print ans
