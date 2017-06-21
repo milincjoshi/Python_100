81614e390442981ec49feb16b85b422950daf7b6
Milin Joshi <milincjoshi@gmail.com>
2017-06-20 22:19:39 -0700

    28

---
+++ 28.py
@@ -0,0 +1,12 @@
+'''
+Question:
+Define a function that can receive two integral numbers in string form and compute their sum and then print it in console.
+
+Hints:
+
+Use int() to convert a string to integer.
+'''
+
+def sum(a,b):
+	return int(a)+int(b)
+print sum("2","4")
