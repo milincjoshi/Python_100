88640a489a42dc65880dcff0e4b4b82248306a88
Milin Joshi <milincjoshi@gmail.com>
2017-06-20 22:15:32 -0700

    26

---
+++ 26.py
@@ -0,0 +1,10 @@
+'''
+Question:
+Define a function which can compute the sum of two numbers.
+
+Hints:
+Define a function with two numbers as arguments. You can compute the sum in the function and return the value.
+'''
+def sum(a,b):
+	return a+b
+print sum(2,3)
