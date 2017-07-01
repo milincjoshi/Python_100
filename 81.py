b5827f6dc5749d1f625a1c13c8ff0aec3bd1db68
Milin Joshi <milincjoshi@gmail.com>
2017-07-01 01:52:41 -0700

    81

--- 81.py
+++ 81.py
@@ -4,3 +4,9 @@ Please write a program to print the running time of execution of "1+1" for 100 t
 Hints:
 Use timeit() function to measure the running time.
 '''
+from timeit import Timer
+def func():
+    for i in range(0,100):
+        1+1
+t = Timer(func)
+print t.timeit()
