1a638422c0e7865e973f15f058dbdd3603efba8e
Milin Joshi <milincjoshi@gmail.com>
2017-06-30 20:32:34 -0700

    70

---
+++ 69.py
@@ -0,0 +1,10 @@
+'''
+Question:
+Please write assert statements to verify that every number in the list [2,4,6,8] is even.
+Hints:
+Use "assert expression" to make assertion.
+'''
+
+l = [2,4,6,8,9]
+for each_val in l:
+    assert each_val%2 == 0
