19e183e75441148747827c9d1ed1df38665f1889
Milin Joshi <milincjoshi@gmail.com>
2017-06-30 20:27:23 -0700

    67

---
+++ 67.py
@@ -0,0 +1,22 @@
+'''
+Question:
+Please write a program using generator 
+to print the even numbers between 0 and n in comma separated
+form while n is input by console.
+Example:
+If the following n is given as input to the program:
+10
+Then, the output of the program should be:
+0,2,4,6,8,10
+Hints:
+Use yield to produce the next value in generator.
+In case of input data being supplied to the question, 
+it should be assumed to be a console input.
+'''
+
+def gen(n):
+    for x in range(0,n+1,2):
+        yield x
+g = gen(10)
+for each_value in g:
+    print each_value
