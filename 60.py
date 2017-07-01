e5f6dc7753469d841f6be776da418d8ed003af3c
Milin Joshi <milincjoshi@gmail.com>
2017-06-30 19:53:05 -0700

    60

--- 60.py
+++ 60.py
@@ -4,6 +4,8 @@ Print a unicode string "hello world".
 Hints:
 Use u'strings' format to define unicode string.
 '''
-x = u'hello world'
+
+y = "hello world"
+x = y.decode("utf-8")
 print x
 print type(x)
